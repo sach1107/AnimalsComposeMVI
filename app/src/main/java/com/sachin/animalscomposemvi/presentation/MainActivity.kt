b7509b02063255b7c6000e37f73c4a9931576d9a
Sachin Kanodia <sackanod@WKMZT733A04F.global.publicisgroupe.net>
2023-07-10 13:28:05 +0530

    update gradle file

--- app/src/main/java/com/sachin/animalscomposemvi/presentation/MainActivity.kt
+++ app/src/main/java/com/sachin/animalscomposemvi/presentation/MainActivity.kt
@@ -1,20 +1,66 @@
-package com.sachin.animalscomposemvi
+package com.sachin.animalscomposemvi.presentation
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.lifecycleScope
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberAsyncImagePainter
+import coil.compose.rememberImagePainter
+import com.sachin.animalscomposemvi.R
+import com.sachin.animalscomposemvi.domain.model.Animal
+import com.sachin.animalscomposemvi.presentation.intent.MainIntent
+import com.sachin.animalscomposemvi.presentation.state.MainState
 import com.sachin.animalscomposemvi.ui.theme.AnimalsComposeMVITheme
+import com.sachin.animalscomposemvi.util.Constants
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val onButtonClick: () -> Unit = {
+            lifecycleScope.launch {
+                viewModel.userIntent.send(MainIntent.FetchAnimals)
+            }
+        }
+
         setContent {
             AnimalsComposeMVITheme {
                 // A surface container using the 'background' color from the theme
@@ -22,7 +68,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainScreen(vm = viewModel, onButtonClick = onButtonClick)
                 }
             }
         }
@@ -30,17 +76,75 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen(vm: MainViewModel, onButtonClick: () -> Unit) {
+
+    when (val state = vm.state.value) {
+        is MainState.Idle -> IdleScreen(onButtonClick)
+        is MainState.Loading -> LoadingScreen()
+        is MainState.Success -> AnimalsList(animals = state.animals)
+        is MainState.Error -> {
+            IdleScreen(onButtonClick)
+            Toast.makeText(LocalContext.current, state.errorMessage, Toast.LENGTH_SHORT).show()
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    AnimalsComposeMVITheme {
-        Greeting("Android")
+fun IdleScreen(onButtonClick: () -> Unit) {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Button(onClick = onButtonClick) {
+            Text(text = "Fetch Animals")
+        }
+    }
+}
+
+@Composable
+fun LoadingScreen() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        CircularProgressIndicator()
+    }
+}
+
+@Composable
+fun AnimalsList(animals: List<Animal>) {
+    LazyColumn {
+        items(items = animals) {
+            AnimalItem(animal = it)
+            Divider(color = Color.LightGray, modifier = Modifier.padding(top = 4.dp, bottom = 4.dp))
+        }
+    }
+}
+
+@OptIn(ExperimentalCoilApi::class)
+@Composable
+fun AnimalItem(animal: Animal) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(60.dp)
+    ) {
+        Spacer(modifier = Modifier.padding(start = 16.dp))
+        val url = Constants.IMAGE_URL + animal.image
+        Image(
+            painter = rememberAsyncImagePainter(
+                model = url,
+                placeholder = painterResource(id = R.drawable.baseline_image_24),
+                error = painterResource(id = R.drawable.baseline_image_24),
+            ),
+            contentDescription = null,
+            modifier = Modifier
+                .size(32.dp)
+                .align(Alignment.CenterVertically),
+            contentScale = ContentScale.FillHeight
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(start = 16.dp),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = animal.name, fontWeight = FontWeight.Bold)
+            Text(text = animal.location)
+        }
     }
 }
