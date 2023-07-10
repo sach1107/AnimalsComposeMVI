33d1e2e378c6e4a3da9611e4ea37b13375a8891e
Sachin Kanodia <sackanod@WKMZT733A04F.global.publicisgroupe.net>
2023-07-10 13:26:47 +0530

    Add view model

--- app/src/main/java/com/sachin/animalscomposemvi/presentation/MainViewModel.kt
+++ app/src/main/java/com/sachin/animalscomposemvi/presentation/MainViewModel.kt
@@ -1,4 +1,49 @@
 package com.sachin.animalscomposemvi.presentation
 
-class MainViewModel {
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sachin.animalscomposemvi.domain.repository.AnimalRepository
+import com.sachin.animalscomposemvi.presentation.intent.MainIntent
+import com.sachin.animalscomposemvi.presentation.state.MainState
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val repo: AnimalRepository
+) : ViewModel() {
+
+    val userIntent = Channel<MainIntent>(Channel.UNLIMITED)
+    var state = mutableStateOf<MainState>(MainState.Idle)
+        private set
+
+    init {
+        handleIntent()
+    }
+
+    private fun handleIntent() {
+        viewModelScope.launch {
+            userIntent.consumeAsFlow().collect { collector ->
+                when (collector) {
+                    is MainIntent.FetchAnimals -> fetchAnimals()
+                }
+            }
+        }
+    }
+
+    private fun fetchAnimals() {
+        viewModelScope.launch {
+            state.value = MainState.Loading
+            state.value = try {
+                MainState.Success(repo.getAnimals())
+            } catch (e: Exception) {
+                MainState.Error(e.localizedMessage)
+            }
+        }
+    }
 }
